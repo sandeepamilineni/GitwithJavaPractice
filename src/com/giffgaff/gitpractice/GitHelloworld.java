f4f59aecb26a591158c5849d6f5f09f6ddd499a6
= <=sandeep.a@nttdata.com>
2020-02-12 17:50:28 +0530

    master: changes from

--- src/com/giffgaff/gitpractice/GitHelloworld.java
+++ src/com/giffgaff/gitpractice/GitHelloworld.java
@@ -2,9 +2,13 @@ package com.giffgaff.gitpractice;
 
 public class GitHelloworld {
 
-	static int count =0;
+	static int count= 0;
+	
 	public static void main(String[] args) {
-		System.out.println("Git practice with Java source"+count++);
+		
+		System.out.println("Git practice with Java source"+ count++);
+		
 	}
 
 }
+
